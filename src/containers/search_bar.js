6ec68ec522ccad1ed2eef14b9d8bcbb691df9116
“Vilde <vilde@vevatne.no>
2017-08-01 11:20:16 -0700

    Add onInputChange function

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+
+		// bind the context of onInputChange
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		// the value of 'this' has a mystery context
+		this.setState({ term: event.target.value });
+	}
+
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input
+					placeholder="Get a five-day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">
 						Submit
