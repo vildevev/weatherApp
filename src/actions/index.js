9d58fa15ffbe0a2d7d99c30edeb5ce93770c2f6c
“Vilde <vilde@vevatne.no>
2017-08-02 10:20:24 -0700

    Add root url and axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 const API_KEY = 'f4a33d0900bf2f8766f2875833144727';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// middlewares = actions pass through them before going to the reducers
 
-// middlewars = actions pass through them before going to the reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	// & for adding additional query
+	const url = `${ROOT_URL}&q=${city},us`;
+	return {
+		type: FETCH_WEATHER
+	};
+}
