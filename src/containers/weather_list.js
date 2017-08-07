517b674c3cf58deda543ab4955390d4095c141fe
“Vilde <vilde@vevatne.no>
2017-08-07 11:14:58 -0700

    Add pressure property

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,6 +6,8 @@ class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 
 		return (
 			<tr key={name}>
@@ -13,7 +15,13 @@ class WeatherList extends Component {
 					{name}
 				</td>
 				<td>
-					<Chart data={temps} color="orange" />
+					<Chart data={temps} color="orange" units="K" />
+				</td>
+				<td>
+					<Chart data={pressures} color="green" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humidities} color="black" units="%" />
 				</td>
 			</tr>
 		);
@@ -25,9 +33,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
